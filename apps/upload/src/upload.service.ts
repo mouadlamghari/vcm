71704c43386ce666c6299c4261b9336d0c0de3ba
mouadlamghari <142666513+mouadlamghari@users.noreply.github.com>
2024-07-12 15:25:31 +0100

    Update upload.service.ts

--- apps/upload/src/upload.service.ts
+++ apps/upload/src/upload.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable,Logger } from '@nestjs/common';
 import simpleGit from 'simple-git';
 import * as fs from 'fs';
 import * as path from 'path';
@@ -8,10 +8,12 @@ import { CommonService } from '@app/common';
 @Injectable()
 export class UploadService {
 
- 
+   private readonly logger = new Logger(UploadService.name); // Create a logger instance
+
   constructor( private commonService : CommonService,@Inject('DEPLOY') private rabbitClient : ClientProxy ){}
 
   async upload(data) {
+         this.logger.log(`Starting upload for repo: ${data.url} with id: ${data.id}`);
     const files = await this.getRepoFiles(data.url,data.id);
     const configFileExists =  files.find(e=>path.basename(e)==='vcm.json')
     if(!configFileExists){
@@ -22,7 +24,7 @@ export class UploadService {
             {status:406,message:'the vcm config file does not exists'}
             ,HttpStatus.NOT_ACCEPTABLE)
     }
-   console.log({files});
+           this.logger.log(`Uploading file: ${files}`);
     await Promise.all(files.map(async(file)=>await this.commonService.uplodFile(file)))
     .then((e)=>{
      console.log(e)
