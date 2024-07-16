1154e2c05d0162bebf005cc97b05738d344d075f
mouadlamghari <142666513+mouadlamghari@users.noreply.github.com>
2024-07-16 22:19:38 +0100

    pppp

--- apps/entrypoint/src/entrypoint.controller.ts
+++ apps/entrypoint/src/entrypoint.controller.ts
@@ -6,7 +6,7 @@ import createUpdate from './dto/upload.dto';
 export class EntrypointController {
   constructor(private readonly entrypointService: EntrypointService) {}
 
-  @Post('/')
+  @Post('/deploy')
   deploy(@Body() UploadBody:createUpdate){
     return this.entrypointService.deploy(UploadBody.repoUrl);
   }
