61eeecdb4ec5025a5dccb983f20caf2713c7cc9b
mouadlamghari <142666513+mouadlamghari@users.noreply.github.com>
2024-07-16 21:23:56 +0100

    err fix

--- apps/entrypoint/src/middleware/middleware.ts
+++ apps/entrypoint/src/middleware/middleware.ts
@@ -11,7 +11,7 @@ export class FallbackMiddleware implements NestMiddleware {
 
   async use(req: Request, res: Response, next: NextFunction) {
     console.log('-----------------------')
-    console.log(req.originalUrl,rea.method);
+    console.log(req.originalUrl,req.method);
     if (!req.originalUrl.match('/') && req.method !== 'POST') {
       const { hostname } = req;
       const instance = await this.commonService.findOne(hostname);
