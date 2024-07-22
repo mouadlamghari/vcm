1360ef69e60a31984bd720f9f228cc8763d69ff5
mouadlamghari <142666513+mouadlamghari@users.noreply.github.com>
2024-07-22 21:29:17 +0100

    .

--- apps/entrypoint/src/entrypoint.controller.ts
+++ apps/entrypoint/src/entrypoint.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards, Req } from '@nestjs/common';
 import { EntrypointService } from './entrypoint.service';
 import createUpdate from './dto/upload.dto';
 import { AuthGuard } from '@nestjs/passport';
@@ -11,7 +11,7 @@ export class EntrypointController {
   @UseGuards(AuthGuard('github'))
   @Post('/deploy')
   deploy(@Req() req: Request, @Body() uploadBody: createUpdate){
-    const user = req.user;
+    const user = req?.user;
     return this.entrypointService.deploy(UploadBody.repoUrl,user);
   }
   @Post('/web')
